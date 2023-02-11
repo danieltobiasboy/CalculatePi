21e8ad77596be8c474236efdefd2febaebf14f8f
Daniel Boy <daniel.boy@studium.fernuni-hagen.de>
2023-02-11 13:50:48 +0100

    Added documentation. Fixed naming.

--- src/main/java/org/tinyprojects/CalculatePiMain.java
+++ src/main/java/org/tinyprojects/CalculatePiMain.java
@@ -1,6 +1,18 @@
 package org.tinyprojects;
 
+/**
+ * Calculates PI to the nth decimal place.
+ */
 public class CalculatePiMain {
+
+    /**
+     * The main method of CalculatePi uses a method from a utility class to calculate
+     * PI to the nth decimal place (rounded) and prints the result to console.
+     * Accurate up to the 8th place.
+     *
+     * @param args like all main method this one takes a String array as a parameter. But any parameter
+     *             except a single int for n will result in a failure message.
+     */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Missing argument. This app calculates PI to the nth decimal. Execute via main -n.");
@@ -17,6 +29,6 @@ public class CalculatePiMain {
             System.out.println("Wrong number format argument. This app calculates PI to the nth decimal. Execute via main -n.");
             return;
         }
-        System.out.println(CalculatePiUtils.calculatePiToNthDecimalPoint(n));
+        System.out.println(CalculatePiUtils.calculatePiToNthDecimalPlace(n));
     }
 }
--- src/main/java/org/tinyprojects/CalculatePiUtils.java
+++ src/main/java/org/tinyprojects/CalculatePiUtils.java
@@ -1,22 +1,34 @@
 package org.tinyprojects;
-
+/**
+ * Utility class for CalculatePiMain. The approximation accuracy can be modified via the fields
+ * MIN_ACCURACY and MAX_ACCURACY.
+ */
 public abstract class CalculatePiUtils {
-    private static long MIN_ACCURACY = 10000000;
-    private static long MAX_ACCURACY = 1000000000;
+    private final static long MIN_ACCURACY = 10000000;
+    private final static long MAX_ACCURACY = 1000000000;
 
-    public static double calculatePiToNthDecimalPoint(int n) {
+    /**
+     * This method calculates an approximate solution for PI via the Leibniz series.
+     * The main method of CalculatePi calculates PI to the nth decimal place and prints the result to console.
+     * This setup enables correct results up to the 8th place.
+     *
+     * @param n The decimal place to which we round PI. Computational complexity rises with n but is bounded by
+     *          MIN_ACCURACY and MAX_ACCURACY.
+     */
+    public static double calculatePiToNthDecimalPlace(int n) {
         double result;
-        long accuracy = Math.round(Math.pow(n+2,n+1));
+        long accuracy = Math.round(Math.pow(n + 2, n + 1));
+        //accuracy bounded checks
         if (accuracy < MIN_ACCURACY) accuracy = MIN_ACCURACY;
         if (accuracy > MAX_ACCURACY) accuracy = MAX_ACCURACY;
+
         double myPi = calcPi(accuracy);
 
         if (n < 1) return Math.round(myPi);
         //round to the nth decimal
-        result = myPi * (Math.pow(10,n));
+        result = myPi * (Math.pow(10, n));
         result = Math.round(result);
-        System.out.println(result);
-        result /= Math.pow(10,n);
+        result /= Math.pow(10, n);
 
         return result;
     }
